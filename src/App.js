5979dff241ecb10c1b600fd2abad9c18dd42110b
Mohd Imran <mohd78696321@gmail.com>
2021-05-19 15:22:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { useState, useRef } from "react";
+import "./Styles/App.scss";
+// Adding Components
+import Song from "./Components/Song";
+import Player from "./Components/Player";
+import Library from "./Components/Library";
+import Nav from "./Components/Nav";
+// import data
+import data from "./data";
+
+function App() {
+  // Ref
+  const audioRef = useRef(null);
+  // State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+    animationPercentage: 0,
+  });
+  const [libraryStatus, setLibraryStatus] = useState(false);
+
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    // Calculate Percentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    console.log(animation);
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage: animation,
+    });
+  };
+  const songEndHandler = async () => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+    if (isPlaying) audioRef.current.play();
+  };
+  return (
+    <div className={`App ${libraryStatus ? "libraryActive" : ""}`}>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      <Song currentSong={currentSong} />
+      <Player
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        audioRef={audioRef}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        setSongs={setSongs}
+      />
+      <Library
+        audioRef={audioRef}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        isPlaying={isPlaying}
+        setSongs={setSongs}
+        libraryStatus={libraryStatus}
+      />
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+        onEnded={songEndHandler}
+      ></audio>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,118 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillhop() {
+  return [
+    {
+      name: "Chill",
+      artist: "Prznt",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/03/75adfe0661d06a9ea66d9c2e99b31e92ae450ebe-150x150.jpg",
+      color: ["#F3AE2C", "#CA553F"],
+      audio:
+        "https://cdn.pixabay.com/download/audio/2021/04/07/audio_8ed06844ef.mp3?filename=nightlife-michael-kobrin-95bpm-3783.mp3",
+      active: true,
+      id: uuidv4(),
+    },
+    {
+      name: "Cruising",
+      artist: "Evil Needle",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/03/74d62bc9370a68e440c1b98eaf650344f0a7faea-1024x1024.jpg",
+      color: ["#283434", "#8F4F2F"],
+      audio:
+        "https://cdn.pixabay.com/download/audio/2021/05/01/audio_f88aa31493.mp3?filename=tech-house-loop-01-4230.mp3",
+      active: false,
+      id: uuidv4(),
+    },
+    {
+      name: "Pop",
+      artist: "Dr. Dre",
+      cover:
+        "https://i.pinimg.com/736x/e6/c2/c3/e6c2c32de43c8d5e7e2a5978be7fea76.jpg",
+      color: ["#54051D", "#21173C"],
+      audio:
+        "https://cdn.pixabay.com/download/audio/2021/03/26/audio_a0e21136cd.mp3?filename=background-loop-funky-04-3515.mp3",
+      active: false,
+      id: uuidv4(),
+    },
+    {
+      name: "Royal Rumble",
+      artist: "Pop Smoke",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/03/7945133f0b1f4f167f056fe0c369f05f2ef415f2-1024x1024.jpg",
+      color: ["#CC5949", "#FAEE78"],
+      audio:
+        "https://cdn.pixabay.com/download/audio/2020/10/11/audio_746c5a0fb3.mp3?filename=awakening-instrumental-1165.mp3",
+      active: false,
+      id: uuidv4(),
+    },
+    {
+      name: "Hip-Hop",
+      artist: "The Evil Man",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/d12927eedcc2f5afba2ab049a4a1ea46c2266ae3-1024x1024.jpg",
+      color: ["#503564", "#57396A"],
+      audio:
+        "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/ArlUHnr1DA6MM4ZemdXBtYrPx1QzyhSPBxs3vMu3.mp3",
+      active: false,
+      id: uuidv4(),
+    },
+    {
+      name: "Night",
+      artist: "Jack",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/70a56749b8b89815fa75446030c6ba57d2c34de7-1024x1024.jpg",
+      color: ["#083747", "#75C09F"],
+      audio:
+        "https://cdn.pixabay.com/download/audio/2021/01/29/audio_61bb0ef4db.mp3?filename=background-loop-chill-techno-01-2483.mp3",
+      active: false,
+      id: uuidv4(),
+    },
+    {
+      name: "Tech House Loop",
+      artist: "Zen Man",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/7f102bdde417f6ead9a120b2b931449e5c12b4da-1024x1024.jpg",
+      color: ["#4C5F33", "#191B10"],
+      audio:
+        "https://cdn.pixabay.com/download/audio/2021/05/01/audio_f88aa31493.mp3?filename=tech-house-loop-01-4230.mp3",
+      active: false,
+      id: uuidv4(),
+    },
+    {
+      name: "Crazy",
+      artist: "Toby Layne",
+      cover:
+        "https://images.pexels.com/photos/4629625/pexels-photo-4629625.jpeg?cs=srgb&dl=pexels-cottonbro-4629625.jpg&fm=jpg",
+      color: ["#A53098", "#7102F3"],
+      audio:
+        "https://cdn.pixabay.com/download/audio/2021/04/21/audio_0230d7579d.mp3?filename=crazy-120bpm-gmin-4059.mp3",
+      active: false,
+      id: uuidv4(),
+    },
+    {
+      name: "Stealth Escape",
+      artist: "Zen Moore",
+      cover:
+        "https://images.pexels.com/photos/2191013/pexels-photo-2191013.jpeg?cs=srgb&dl=pexels-lopsan-2191013.jpg&fm=jpg",
+      color: ["#0C0B0C", "#421C1C"],
+      audio:
+        "https://cdn.pixabay.com/download/audio/2021/04/08/audio_cd16379edf.mp3?filename=background-loop-melodic-techno-04-3822.mp3",
+      active: false,
+      id: uuidv4(),
+    },
+    {
+      name: "Innovation",
+      artist: "John Sib",
+      cover:
+        "https://images.pexels.com/photos/811838/pexels-photo-811838.jpeg?cs=srgb&dl=pexels-edward-eyer-811838.jpg&fm=jpg",
+      color: ["#121212", "#919191"],
+      audio:
+        "https://cdn.pixabay.com/download/audio/2021/04/07/audio_60a6b1f99c.mp3?filename=innovation-3794.mp3",
+      active: false,
+      id: uuidv4(),
+    },
+  ];
+}
+
+export default chillhop;
